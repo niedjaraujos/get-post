07ad97a9fd5e0312adb882018f08b4588703983c
niedjaraujos <naraujos1007@hotmail.com>
2026-03-13 23:55:40 -0300

    desafio-api

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+let voluntarios = [];
+
+app.post("/voluntarios", (req, res) => {
+  const { nome, email, telefone, mensagem, especialidade } = req.body;
+
+  //  Nome com no mínimo 3 caracteres
+  if (!nome || nome.length < 3) {
+    return res
+      .status(400)
+      .json({ erro: "Nome deve ter no mínimo 3 caracteres." });
+  }
+
+  // Email válido (simples)
+  if (!email || !email.includes("@")) {
+    return res.status(400).json({ erro: "E-mail inválido." });
+  }
+
+  // Telefone com 10 ou 11 números
+  const telCampo = telefone.replace(/\D/g, "");
+  if (telCampo.length < 10 || telCampo.length > 11) {
+    return res
+      .status(400)
+      .json({ erro: "Telefone deve ter 10 ou 11 números." });
+  }
+
+  // Mensagem com no máximo 500 caracteres
+  if (mensagem && mensagem.length > 500) {
+    return res
+      .status(400)
+      .json({ erro: "Mensagem muito longa (máx 500 caracteres)." });
+  }
+
+  const novoVoluntario = {
+    id: voluntarios.length + 1,
+    nome,
+    email,
+    telefone: telCampo,
+    mensagem,
+    especialidade,
+  };
+  voluntarios.push(novoVoluntario);
+
+  return res.status(201).json(novoVoluntario);
+});
+
+app.get("/voluntarios", (req, res) => {
+  res.status(200).json(voluntarios);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodandp na porta http://localhost:${PORT}`);
+});
